97a03a7ddda2e570a5369809b297fedd86a68267
Colin Leary <colinleary123@gmail.com>
2024-10-15 22:28:41 -0400

    Table

--- src/PeriodicTable.jsx
+++ src/PeriodicTable.jsx
@@ -1,26 +1,29 @@
+// PeriodicTable.jsx
 import React from "react";
 import { mergePeriodicTableData } from "./mergePeriodicTableData";
-import "./periodicTable.css"; // Import the dedicated CSS file for the periodic table
+import "./periodicTable.css";
 
 function PeriodicTable() {
-  const elements = mergePeriodicTableData(); // Call the function to get merged data
+  const elements = mergePeriodicTableData(); // Get the merged data
 
   return (
-    <div className="periodic-table">
-      {elements.map((element) => (
-        <div
-          key={element.symbol}
-          className={`element ${element.category}`} // Adding a class based on the category of the element
-          style={{
-            gridColumn: element.xpos,
-            gridRow: element.ypos,
-          }}
-        >
-          <span className="symbol">{element.symbol}</span>
-          <span className="number">{element.number}</span>
-          <span className="name">{element.name}</span>
-        </div>
-      ))}
+    <div className="periodic-table-wrapper">
+      <div className="periodic-table">
+        {elements.map((element) => (
+          <div
+            key={element.symbol}
+            className={`element ${element.category}`}
+            style={{
+              gridColumn: element.xpos,
+              gridRow: element.ypos,
+            }}
+          >
+            <span className="symbol">{element.symbol}</span>
+            <span className="number">{element.number}</span>
+            <span className="name">{element.name}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
